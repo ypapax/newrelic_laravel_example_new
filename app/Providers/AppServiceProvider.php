4b1670a9c5ba0ad352563795c7ba727aa00c4290
Maxim Yefremov <maxYefr@gmail.com>
2019-10-08 07:39:04 +0300

    attempt to register newrelic app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Intouch\Newrelic\Newrelic;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $nr = new Newrelic();
+        $nr->setAppName('MyApp');
     }
 
     /**
